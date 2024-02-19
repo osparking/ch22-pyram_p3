062a9b1988831cb8943967693609f5b7d3514903
osparking <jbpark03@gmail.com>
2024-02-19 18:28:46 +0900

    집적 시험 갱신 1차 - 변수 선언

--- src/test/java/space/bum/junit/pyram_p3/airport/FlightWithPassengersTest.java
+++ src/test/java/space/bum/junit/pyram_p3/airport/FlightWithPassengersTest.java
@@ -3,6 +3,8 @@ package space.bum.junit.pyram_p3.airport;
 import static org.junit.Assert.assertEquals;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 import javax.inject.Inject;
 
@@ -11,8 +13,12 @@ import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.asset.EmptyAsset;
 import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnit;
+import org.mockito.junit.MockitoRule;
 
 import space.bum.junit.pyram_p3.airport.producers.FlightProducer;
 
@@ -27,7 +33,16 @@ public class FlightWithPassengersTest {
   }
 
   @Inject
+  @FlightNumber(number = "AB5422")
   Flight flight;
+
+  @Mock
+  DistanceManager distanceManager;
+
+  @Rule
+  public MockitoRule mockitoRule = MockitoJUnit.rule();
+
+  private static Map<Passenger, Integer> passengersPointsMap = new HashMap<>();
 
   @Test(expected = RuntimeException.class)
   public void testNumberOfSeatsCannotBeExceeded() throws IOException {

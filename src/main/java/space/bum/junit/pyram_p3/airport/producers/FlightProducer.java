b2bed55fdfac7640d50b579aa895218167fb4770
osparking <jbpark03@gmail.com>
2024-02-19 17:35:29 +0900

    FlightNumber 애노 활용-일반화된 buildFromCsv 사용

--- src/main/java/space/bum/junit/pyram_p3/airport/producers/FlightProducer.java
+++ src/main/java/space/bum/junit/pyram_p3/airport/producers/FlightProducer.java
@@ -6,10 +6,13 @@ import javax.enterprise.inject.Produces;
 
 import space.bum.junit.pyram_p3.airport.Flight;
 import space.bum.junit.pyram_p3.airport.FlightBuilderUtil;
+import space.bum.junit.pyram_p3.airport.FlightNumber;
 
 public class FlightProducer {
   @Produces
+  @FlightNumber(number = "OZ567")
   public Flight createFlight() throws IOException {
-    return FlightBuilderUtil.buildFlightFromCsv();
+    return FlightBuilderUtil.buildFlightFromCsv("KE007", 50,
+        "src/test/resources/flights_information.csv");
   }
 }

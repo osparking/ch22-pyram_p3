60f27af41dd1de8f0ea24075f535dfd4206a9e9e
osparking <jbpark03@gmail.com>
2024-02-19 12:50:26 +0900

    더미/모조 거리 관리자

---
+++ src/main/java/space/bum/junit/pyram_p3/airport/DistanceManager.java
@@ -0,0 +1,19 @@
+package space.bum.junit.pyram_p3.airport;
+
+import java.util.Map;
+
+public class DistanceManager {
+  public Map<Passenger, Integer> getPassengersDistancesMap() {
+    return null;
+  }
+
+  public Map<Passenger, Integer> getPassengersPointsMap() {
+    return null;
+  }
+
+  public void addDistance(Passenger passenger, int distance) {
+  }
+
+  public void calculateGivenPoints() {
+  }
+}
